10472d97e11f2d1ae9dc295dc3d326d8f2c82cbd
David Medina <davidamedina619@gmail.com>
2020-09-22 18:07:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,12 @@ def reverse_each_word(sentence1)
  sentence1.each { |x| puts x.reverse }
 
 end 
+
+def reverse_each_word(sentence)
+  words_array = sentence.split(' ')
+  new_array = []
+  words_array.each do |word|
+  new_array << word.reverse
+  end
+return new_array.join(' ')
+end
